7060f276030539d255ebf46cead0080cef9d4cd0
Big R <confusing.big@gmail.com>
2022-05-13 19:50:06 +0700

    Program works as expectgit add .!

--- glyphs.py
+++ glyphs.py
@@ -1,168 +1,170 @@
-if dee[0] == "ก":
-    dee[0] = "d"
-elif dee[0] == "ข":
-    dee[0] = "-"
-elif dee[0] == "ค":
-    dee[0] = "8"
-elif dee[0] == "ฅ":
-    dee[0] = "8"
-elif dee[0] == "ฆ":
-    dee[0] = "S"
-elif dee[0] == "ง":
-    dee[0] = "'"
-elif dee[0] == "จ":
-    dee[0] = "0"
-elif dee[0] == "ฉ":
-    dee[0] = "C"
-elif dee[0] == "ช":
-    dee[0] = "="
-elif dee[0] == "ซ":
-    dee[0] = ":"
-elif dee[0] == "ฌ":
-    dee[0] = "G"
-elif dee[0] == "ญ":
-    dee[0] = "P"
-elif dee[0] == "ฎ":
-    dee[0] = "E"
-elif dee[0] == "ฏ":
-    dee[0] = "D"
-elif dee[0] == "ฐ":
-    dee[0] = "{"
-elif dee[0] == "ฑ":
-    dee[0] = "R"
-elif dee[0] == "ฒ":
-    dee[0] = "<"
-elif dee[0] == "ณ":
-    dee[0] = "I"
-elif dee[0] == "ด":
-    dee[0] = "f"
-elif dee[0] == "ต":
-    dee[0] = "9"
-elif dee[0] == "ถ":
-    dee[0] = "5"
-elif dee[0] == "ท":
-    dee[0] = "m"
-elif dee[0] == "ธ":
-    dee[0] = "T"
-elif dee[0] == "น":
-    dee[0] = "o"
-elif dee[0] == "บ":
-    dee[0] = "["
-elif dee[0] == "ป":
-    dee[0] = "x"
-elif dee[0] == "ผ":
-    dee[0] = "z"
-elif dee[0] == "ฝ":
-    dee[0] = "/"
-elif dee[0] == "พ":
-    dee[0] = "r"
-elif dee[0] == "ฟ":
-    dee[0] = "a"
-elif dee[0] == "ภ":
-    dee[0] = "4"
-elif dee[0] == "ม":
-    dee[0] = ","
-elif dee[0] == "ย":
-    dee[0] = "p"
-elif dee[0] == "ร":
-    dee[0] = "i"
-elif dee[0] == "ฤ":
-    dee[0] = "A"
-elif dee[0] == "ล":
-    dee[0] = "]"
-elif dee[0] == "ฦ":
-    dee[0] = "?"
-elif dee[0] == "ว":
-    dee[0] = ";"
-elif dee[0] == "ศ":
-    dee[0] = "L"
-elif dee[0] == "ษ":
-    dee[0] = "K"
-elif dee[0] == "ส":
-    dee[0] = "l"
-elif dee[0] == "ห":
-    dee[0] = "s"
-elif dee[0] == "ฬ":
-    dee[0] = ">"
-elif dee[0] == "อ":
-    dee[0] = "v"
-elif dee[0] == "ฮ":
-    dee[0] = "V"
-elif dee[0] == "ฯ":
-    dee[0] = "O"
-elif dee[0] == "ะ":
-    dee[0] = "t"
-elif dee[0] == "ั":
-    dee[0] = "y"
-elif dee[0] == "า":
-    dee[0] = ""
-elif dee[0] == "ำ":
-    dee[0] = "k"
-elif dee[0] == "ิ":
-    dee[0] = "b"
-elif dee[0] == "ี":
-    dee[0] = "u"
-elif dee[0] == "ึ":
-    dee[0] = "7"
-elif dee[0] == "ื":
-    dee[0] = "n"
-elif dee[0] == "ุ":
-    dee[0] = "6"
-elif dee[0] == "ู":
-    dee[0] = "^"
-elif dee[0] == "ฺ":
-    dee[0] = "B"
-elif dee[0] == "เ":
-    dee[0] = "g"
-elif dee[0] == "แ":
-    dee[0] = "c"
-elif dee[0] == "โ":
-    dee[0] = "F"
-elif dee[0] == "ใ":
-    dee[0] = "."
-elif dee[0] == "ไ":
-    dee[0] = "w"
-elif dee[0] == "ๅ":
-    dee[0] = "!"
-elif dee[0] == "ๆ":
-    dee[0] = "q"
-elif dee[0] == "็":
-    dee[0] = "H"
-elif dee[0] == "่":
-    dee[0] = "j"
-elif dee[0] == "้":
-    dee[0] = "h"
-elif dee[0] == "๊":
-    dee[0] = "U"
-elif dee[0] == "๋":
-    dee[0] = "J"
-elif dee[0] == "์":
-    dee[0] = "N"
-elif dee[0] == "ํ":
-    dee[0] = "Y"
-elif dee[0] == "๎":
-    dee[0] = ""
-elif dee[0] == "๏":
-    dee[0] = ""
-elif dee[0] == "๐":
-    dee[0] = "Q"
-elif dee[0] == "๑":
-    dee[0] = "@"
-elif dee[0] == "๒":
-    dee[0] = "#"
-elif dee[0] == "๓":
-    dee[0] = "$"
-elif dee[0] == "๔":
-    dee[0] = "%"
-elif dee[0] == "๕":
-    dee[0] = "*"
-elif dee[0] == "๖":
-    dee[0] = "("
-elif dee[0] == "๗":
-    dee[0] = ")"
-elif dee[0] == "๘":
-    dee[0] = "_"
-elif dee[0] == "๙":
-    dee[0] = "+"
-elif dee[0] == "๚":
-    dee[0] = ""
+def glyphsTrans(dee=[], *args):
+    for x in range(len(dee)):
+        if dee[x] == "ก":
+            dee[x] = "d"
+        elif dee[x] == "ข":
+            dee[x] = "-"
+        elif dee[x] == "ค":
+            dee[x] = "8"
+        elif dee[x] == "ฅ":
+            dee[x] = "8"
+        elif dee[x] == "ฆ":
+            dee[x] = "S"
+        elif dee[x] == "ง":
+            dee[x] = "'"
+        elif dee[x] == "จ":
+            dee[x] = "x"
+        elif dee[x] == "ฉ":
+            dee[x] = "C"
+        elif dee[x] == "ช":
+            dee[x] = "="
+        elif dee[x] == "ซ":
+            dee[x] = ":"
+        elif dee[x] == "ฌ":
+            dee[x] = "G"
+        elif dee[x] == "ญ":
+            dee[x] = "P"
+        elif dee[x] == "ฎ":
+            dee[x] = "E"
+        elif dee[x] == "ฏ":
+            dee[x] = "D"
+        elif dee[x] == "ฐ":
+            dee[x] = "{"
+        elif dee[x] == "ฑ":
+            dee[x] = "R"
+        elif dee[x] == "ฒ":
+            dee[x] = "<"
+        elif dee[x] == "ณ":
+            dee[x] = "I"
+        elif dee[x] == "ด":
+            dee[x] = "f"
+        elif dee[x] == "ต":
+            dee[x] = "9"
+        elif dee[x] == "ถ":
+            dee[x] = "5"
+        elif dee[x] == "ท":
+            dee[x] = "m"
+        elif dee[x] == "ธ":
+            dee[x] = "T"
+        elif dee[x] == "น":
+            dee[x] = "o"
+        elif dee[x] == "บ":
+            dee[x] = "["
+        elif dee[x] == "ป":
+            dee[x] = "x"
+        elif dee[x] == "ผ":
+            dee[x] = "z"
+        elif dee[x] == "ฝ":
+            dee[x] = "/"
+        elif dee[x] == "พ":
+            dee[x] = "r"
+        elif dee[x] == "ฟ":
+            dee[x] = "a"
+        elif dee[x] == "ภ":
+            dee[x] = "4"
+        elif dee[x] == "ม":
+            dee[x] = ","
+        elif dee[x] == "ย":
+            dee[x] = "p"
+        elif dee[x] == "ร":
+            dee[x] = "i"
+        elif dee[x] == "ฤ":
+            dee[x] = "A"
+        elif dee[x] == "ล":
+            dee[x] = "]"
+        elif dee[x] == "ฦ":
+            dee[x] = "?"
+        elif dee[x] == "ว":
+            dee[x] = ";"
+        elif dee[x] == "ศ":
+            dee[x] = "L"
+        elif dee[x] == "ษ":
+            dee[x] = "K"
+        elif dee[x] == "ส":
+            dee[x] = "l"
+        elif dee[x] == "ห":
+            dee[x] = "s"
+        elif dee[x] == "ฬ":
+            dee[x] = ">"
+        elif dee[x] == "อ":
+            dee[x] = "v"
+        elif dee[x] == "ฮ":
+            dee[x] = "V"
+        elif dee[x] == "ฯ":
+            dee[x] = "O"
+        elif dee[x] == "ะ":
+            dee[x] = "t"
+        elif dee[x] == "ั":
+            dee[x] = "y"
+        elif dee[x] == "า":
+            dee[x] = ""
+        elif dee[x] == "ำ":
+            dee[x] = "k"
+        elif dee[x] == "ิ":
+            dee[x] = "b"
+        elif dee[x] == "ี":
+            dee[x] = "u"
+        elif dee[x] == "ึ":
+            dee[x] = "7"
+        elif dee[x] == "ื":
+            dee[x] = "n"
+        elif dee[x] == "ุ":
+            dee[x] = "6"
+        elif dee[x] == "ู":
+            dee[x] = "^"
+        elif dee[x] == "ฺ":
+            dee[x] = "B"
+        elif dee[x] == "เ":
+            dee[x] = "g"
+        elif dee[x] == "แ":
+            dee[x] = "c"
+        elif dee[x] == "โ":
+            dee[x] = "F"
+        elif dee[x] == "ใ":
+            dee[x] = "."
+        elif dee[x] == "ไ":
+            dee[x] = "w"
+        elif dee[x] == "ๅ":
+            dee[x] = "!"
+        elif dee[x] == "ๆ":
+            dee[x] = "q"
+        elif dee[x] == "็":
+            dee[x] = "H"
+        elif dee[x] == "่":
+            dee[x] = "j"
+        elif dee[x] == "้":
+            dee[x] = "h"
+        elif dee[x] == "๊":
+            dee[x] = "U"
+        elif dee[x] == "๋":
+            dee[x] = "J"
+        elif dee[x] == "์":
+            dee[x] = "N"
+        elif dee[x] == "ํ":
+            dee[x] = "Y"
+        elif dee[x] == "๎":
+            dee[x] = ""
+        elif dee[x] == "๏":
+            dee[x] = ""
+        elif dee[x] == "๐":
+            dee[x] = "Q"
+        elif dee[x] == "๑":
+            dee[x] = "@"
+        elif dee[x] == "๒":
+            dee[x] = "#"
+        elif dee[x] == "๓":
+            dee[x] = "$"
+        elif dee[x] == "๔":
+            dee[x] = "%"
+        elif dee[x] == "๕":
+            dee[x] = "*"
+        elif dee[x] == "๖":
+            dee[x] = "("
+        elif dee[x] == "๗":
+            dee[x] = ")"
+        elif dee[x] == "๘":
+            dee[x] = "_"
+        elif dee[x] == "๙":
+            dee[x] = "+"
+        elif dee[x] == "๚":
+            dee[x] = ""
