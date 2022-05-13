7bc52c875a30be4edd005bb8e4815c46ad9ff8e8
Big R <confusing.big@gmail.com>
2022-05-13 19:48:36 +0700

    Program works as expect!!!

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
-x = "ืตนชัย จำเริญบุญ"
+from glyphs import *
+x = "กิ่งดาว ภูมิคอนสาร น่ารัก"
 dee = list(x)
 print(dee)
-"""
-yout = chr(ord(x) - 1)
-print(yout)
-print(ord("ั"))
-print(chr(3631))
-for x in range(3584,4000,1):
-    print(x,chr(x))
-"""
-"""
-for x in range(3585,3674,1):
-    if dee[0] == chr(x):
-        print("ก")
-for x in range(3585,3674,1):
-    if dee[1] == chr(x):
-        print("ข")
-"""
-
+glyphsTrans(dee)
 
 
 print(dee)
 print("We're here")
 print(ord("ื"))
 
-#This section is to joining the list
+# This section is to joining the list
 s = ''
 joined = s.join(dee)
 print(joined)
